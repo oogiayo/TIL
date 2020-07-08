513383b8f46c7e22c6569084f9fba030a8ab8be6
oogiayo <donguk.smile@gmail.com>
2020-07-08 21:03:16 +0900

    TIL_0708

---
+++ src/TIL_0708/Solution_d3_10059_유효기간.java
@@ -0,0 +1,30 @@
+package TIL_0708;
+import java.util.*;
+
+public class Solution_d3_10059_유효기간 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int t=1; t<=T; t++) {
+			int N = sc.nextInt();
+			int AB = N/100;
+			int CD = N%100;
+			String ans = "";
+			if(AB<13 && CD<13) {
+				ans = "AMBIGUOUS";
+			}
+			else if(AB<13) {
+				ans = "MMYY";
+			}
+			else if(CD<13) {
+				ans = "YYMM";
+			}
+			else {
+				ans = "NA";
+			}
+			System.out.println("#" + t + " " + ans);
+		}//end of tc
+		
+	}//end of main
+
+}//end of class

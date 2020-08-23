dc0020b3bd9f6d0bec38750c0ece1a9bf76e7bf5
oogiayo <donguk.smile@gmail.com>
2020-08-23 23:46:19 +0900

    Add TIL_0823 algorithm problem

--- src/TIL_0822/Main_bj_2304_창고다각형.java
+++ src/TIL_0822/Main_bj_2304_창고다각형.java
@@ -1,8 +1,8 @@
 package TIL_0822;
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-public class op_Main_bj_2304_창고다각형 {
+public class Main_bj_2304_창고다각형 {
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -10,7 +10,6 @@ public class op_Main_bj_2304_창고다각형 {
 		int[] location = new int[N];
 		int[] height = new int[N];
 		int max_height = 0;
-//		int max_idx = 0;
 		int check = 0;
 		for(int i=0; i<N; i++) {
 			String[] line = bf.readLine().split(" ");
@@ -41,15 +40,15 @@ public class op_Main_bj_2304_창고다각형 {
 		int l = location[0];
 		int h = height[0];
 		while(true) {
+			if(height[idx]==max_height) {
+				break;
+			}
 			idx++;
 			if(height[idx] > h) {
-				area += (location[idx] - l)*h;
+				area += (location[idx]-l)*h;
 				h = height[idx];
 				l = location[idx];
 			}
-			if(height[idx]==max_height) {
-				break;
-			}	
 		}
 		
 		while(true) {
@@ -59,16 +58,27 @@ public class op_Main_bj_2304_창고다각형 {
 			}
 			else {
 				idx++;
-				check--;
+				if(height[idx]==max_height) {
+					check--;					
+				}
 			}
 		}
 		
-//		idx = N-1;
-//		int r = location[N-1];
-//		h = location[N-1];
-//		while(true) {
-//			break;
-//		}
+		idx = N-1;
+		int r = location[N-1];
+		h = height[N-1];
+		
+		while(height[idx]!=max_height) {
+			idx--;
+			if(height[idx] > h) {
+				area += (r - location[idx])*h;
+				h = height[idx];
+				r = location[idx];
+			}
+			if(height[idx]==max_height) {
+				break;
+			}
+		}
 		System.out.println(area);
 		
 	}//end of main
